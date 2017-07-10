612bc6121516641ae777d5c1d9f6f19e087b73a3
Vaibhav <vaibhav@Vaibhavs-Mac-mini.local>
2017-07-10 14:42:19 +0530

    Storing Order in DB

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 var Cart = require('../models/cart');
+var Order = require('../models/order');
 
 router.use(function (req, res, next) {
     res.locals.login = req.isAuthenticated();
@@ -42,7 +43,7 @@ router.get('/shopping-cart', function (req, res, next) {
     var cart = new Cart(req.session.cart.items);
     res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
     if (!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
@@ -51,7 +52,7 @@ router.get('/checkout', function(req, res, next) {
         noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout', isLoggedIn, function(req, res, next) {
     if (!req.session.cart) {
         return res.redirect('/shopping-cart');
     }
@@ -72,10 +73,27 @@ router.post('/checkout', function(req, res, next) {
             req.flash('error', err.message);
             return res.redirect('/checkout');
         }
-        req.flash('success', 'Successfully bought product!');
-        req.session.cart = null;
-        res.redirect('/');
+        var order = new Order({
+            user: req.user,
+            cart: cart,
+            address: req.body.address,
+            name: req.body.name,
+            paymentId: charge.id
+        });
+        order.save(function(err, result) {
+            req.flash('success', 'Successfully bought product!');
+            req.session.cart = null;
+            res.redirect('/');
+        });
     });
 });
 
 module.exports = router;
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    console.log(req.url);
+    res.redirect('/user/signin');
+}
